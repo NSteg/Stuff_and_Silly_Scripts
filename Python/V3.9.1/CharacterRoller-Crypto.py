a8051572df557725e42451ea3358fa2f212486d2
Nick <nstegelman@gmail.com>
2022-05-16 19:39:57 -0600

    Updated Version number.

--- Python/V3.9.1/CharacterRoller-Crypto.py
+++ Python/V3.9.1/CharacterRoller-Crypto.py
@@ -28,7 +28,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-## Ver.0.1.0
+## Ver: 1.0.0
 
 ## Maintainers: Nicholas Stegelman
 # Last Updated: 2022-05-16
